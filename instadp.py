4ac40bb72549c3a6969854ca599b9bfe2dad1744
Akshay Ravi <58636452+AkshayraviC09YC47@users.noreply.github.com>
2022-11-09 19:30:07 +0530

    Update instadp.py

--- instadp.py
+++ instadp.py
@@ -1,56 +1,47 @@
-#usr/bin/env python3
-from os import get_terminal_size
-from colorama import Fore
-from colorama import Style
-from colorama import init
-import instaloader
+from instaloader import Instaloader
 import argparse
-import time
-import sys
-import os
-import io
 
-s=get_terminal_size().columns
-init(autoreset=True)
+banner="""
 
-r=Fore.RED+Style.BRIGHT
-y=Fore.YELLOW+Style.BRIGHT
-b=Fore.BLUE+Style.BRIGHT
-g=Fore.GREEN+Style.BRIGHT
-c=Fore.CYAN+Style.BRIGHT
-m=Fore.MAGENTA+Style.BRIGHT                                                
+ /$$$$$$  /$$$$$$          /$$$$$$$  /$$$$$$$ 
+|_  $$_/ /$$__  $$        | $$__  $$| $$__  $$
+  | $$  | $$  \__/        | $$  \ $$| $$  \ $$
+  | $$  | $$ /$$$$ /$$$$$$| $$  | $$| $$$$$$$/
+  | $$  | $$|_  $$|______/| $$  | $$| $$____/ 
+  | $$  | $$  \ $$        | $$  | $$| $$      
+ /$$$$$$|  $$$$$$/        | $$$$$$$/| $$      
+|______/ \______/         |_______/ |__/      
+                                              
+ #### Instagram Profile Picture Downloader ####                                              
+                                              
+"""
 
-def profile():
-		try:
-			parser=argparse.ArgumentParser()
-			dp=instaloader.Instaloader()
-			parser.add_argument('link',help='Enter the profile username')
-			args=parser.parse_args()
-			os.system("clear")
-			print("\n")
-			print(r+" /$$$$$$  /$$$$$$          /$$$$$$$  /$$$$$$$ ".center(s))
-			print(y+"|_  $$_/ /$$__  $$        | $$__  $$| $$__  $$".center(s))
-			print(b+"  | $$  | $$  \__/        | $$  \ $$| $$  \ $$".center(s))
-			print(g+"  | $$  | $$ /$$$$ /$$$$$$| $$  | $$| $$$$$$$/".center(s))
-			print(r+"  | $$  | $$|_  $$|______/| $$  | $$| $$____/ ".center(s))
-			print(m+"  | $$  | $$  \ $$        | $$  | $$| $$     ".center(s))    
-			print(r+" /$$$$$$|  $$$$$$/        | $$$$$$$/| $$     ".center(s))    
-			print(y+"|______/ \______/         |_______/ |__/      ".center(s))
-			print(c,"\n")
-			print(y+"#### Instagram Profile Picture Downloader By C09YC47 #### \n".center(s))
-			print(c+"[!] Downloading please wait.....")
-			sys.stdout = dump_data = io.StringIO()
-			dp.download_profile(args.link,profile_pic_only=True)
-			os.system("mkdir /sdcard/IGDP> /dev/null 2>&1")
-			os.system("mv "+args.link+" /sdcard/IGDP> /dev/null 2>&1")
-			time.sleep(1)
-			sys.stdout = sys.__stdout__
-			result=(f"[*] Downloaded Successfully And Saved To Folder IGDP as '{args.link}'")
-			print(g+result)
-			
-		except:
-			print(r+"[!] This Pic Has Been Already Downloaded Or Something Went Wrong!")
-			exit()
-		
-profile()
+parser = argparse.ArgumentParser()
 
+parser.add_argument('-u',help ='Enter the username')
+parser.add_argument('-l',help ='Path to the username list text file')
+
+args = parser.parse_args()
+username = args.u
+u_list =args.l
+
+if not any((username,u_list)):
+	print(banner)
+	print("-"*100)
+	print("To download single users profile pic, use this command:   'python3 instadp.py -u sunnyleone'")
+	print("To download multiple users profile pic, use this command: 'python3 instadp.py -l usernamelist.txt'")
+	print("-"*100)
+
+if username:
+	print(banner)
+	print(f"Downloading {username} profile picture, please wait....")
+	Instaloader().download_profile(username, profile_pic_only=True)
+
+if u_list:
+	print(banner)
+	with open(u_list,'r') as f:
+		for line in f:
+			for word in line.split():
+				print(f"Downloading {word} profile picture, please wait....")
+				Instaloader().download_profile(word, profile_pic_only=True)
+				print("-"*50)
